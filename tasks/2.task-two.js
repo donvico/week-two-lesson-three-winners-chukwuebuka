3c77173692a1290cb35b09df5e9f7e48ebf58c86
donvico <wvictoryc@gmail.com>
2023-06-30 05:21:17 +0100

    commit

---
+++ tasks/2.task-two.js
@@ -0,0 +1,5 @@
+const states = ['Abia', 'Anammbra', 'Edo', 'Lagos', 'Ondo'];
+
+for (let i = states.length - 1; i >= 0; i--) {
+  console.log(states[i]); //output reverses in this format= Ondo, Lagos, Edo, Anammbra, Abia
+}
---
+++ tasks/6.task-six.js
@@ -0,0 +1,7 @@
+for (let number = 1; number <= 10; number++) {
+    if (number === 5) {
+      break;
+    }
+    console.log(number);
+  }
+  
